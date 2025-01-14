ff65bb1ccef20a30b5df407ed3a33f6d897dcb13
Gabriel Arturo Félix Páez <gabrielarturo0221@gmail.com>
2025-01-14 19:07:21 -0400

    add .hintrc file and update apple-touch-icon path in layout

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -32,11 +32,10 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* Favicon y configuraciones de íconos */}
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/my-app/public/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <meta name="theme-color" content="#000000" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 text-gray-900`}
